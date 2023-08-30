c3320b4d21f1e77174664ed2debfd78b201f37c1
JR1811 <janreichelt77@gmail.com>
2023-08-30 10:11:39 +0200

    small javadoc addition to explain the new Interface's functionality

--- src/main/java/net/shirojr/titanfabric/util/items/SelectableArrows.java
+++ src/main/java/net/shirojr/titanfabric/util/items/SelectableArrows.java
@@ -1,4 +1,8 @@
 package net.shirojr.titanfabric.util.items;
 
+/**
+ * Items, which implement this Interface, can open the {@linkplain net.shirojr.titanfabric.screen.custom.ArrowSelectionScreen ArrowSelectionScreen}.
+ * This Screen is able to select Arrow Types.
+ */
 public interface SelectableArrows {
 }
