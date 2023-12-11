8e62cd43e6be14bb0b5b4822a172808466fdf7df
JR1811 <janreichelt77@gmail.com>
2023-12-11 08:58:23 +0100

    extracted SlotArrangementType enum to specialize different WeaponEffect recipes

--- src/main/java/net/shirojr/titanfabric/recipe/TitanFabricRecipes.java
+++ src/main/java/net/shirojr/titanfabric/recipe/TitanFabricRecipes.java
@@ -7,14 +7,14 @@ import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 import net.shirojr.titanfabric.TitanFabric;
 import net.shirojr.titanfabric.recipe.custom.DiamondFurnaceRecipe;
-import net.shirojr.titanfabric.recipe.custom.WeaponEffectRecipe;
+import net.shirojr.titanfabric.recipe.custom.EssenceRecipe;
 import net.shirojr.titanfabric.util.LoggerUtil;
 
 public class TitanFabricRecipes {
     public static final RecipeType<DiamondFurnaceRecipe> DIAMOND_FURNACE_RECIPE_TYPE = registerRecipe("diamond_furnace");
-    public static final RecipeType<WeaponEffectRecipe> WEAPON_EFFECT_RECIPE_TYPE = RecipeType.register("weapon_effect");
+    public static final RecipeType<EssenceRecipe> WEAPON_EFFECT_RECIPE_TYPE = RecipeType.register("weapon_effect");
 
-    public static final RecipeSerializer<WeaponEffectRecipe> WEAPON_EFFECT_RECIPE_SERIALIZER = RecipeSerializer.register("weapon_effect", new WeaponEffectRecipe.Serializer());
+    public static final RecipeSerializer<EssenceRecipe> WEAPON_EFFECT_RECIPE_SERIALIZER = RecipeSerializer.register("weapon_effect", new EssenceRecipe.Serializer());
 
     private static <T extends Recipe<?>> RecipeType<T> registerRecipe(String id) {
         return Registry.register(Registry.RECIPE_TYPE, new Identifier(TitanFabric.MODID, id),
