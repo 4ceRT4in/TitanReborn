f8513b0b72539b45927606631966d5a7112f2365
Globox1997 <mitschke11@web.de>
2023-12-12 13:28:14 +0100

    moved to client mixin folder

--- src/main/java/net/shirojr/titanfabric/mixin/client/AbstractClientPlayerEntityMixin.java
+++ src/main/java/net/shirojr/titanfabric/mixin/client/AbstractClientPlayerEntityMixin.java
@@ -1,5 +1,7 @@
-package net.shirojr.titanfabric.mixin;
+package net.shirojr.titanfabric.mixin.client;
 
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
 import net.minecraft.client.network.AbstractClientPlayerEntity;
 import net.minecraft.item.BowItem;
 import net.minecraft.item.Item;
@@ -8,6 +10,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
+@Environment(EnvType.CLIENT)
 @Mixin(AbstractClientPlayerEntity.class)
 public abstract class AbstractClientPlayerEntityMixin {
     @Redirect(method = "getFovMultiplier", at = @At(value = "INVOKE", target = "Lnet/minecraft/item/ItemStack;isOf(Lnet/minecraft/item/Item;)Z"))
