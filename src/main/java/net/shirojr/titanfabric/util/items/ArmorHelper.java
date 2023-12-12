733828039d1c1e33666b7ea48cb3971a906cce99
Globox1997 <mitschke11@web.de>
2023-12-12 19:51:00 +0100

    reworked armor damage handling

---
+++ src/main/java/net/shirojr/titanfabric/util/items/ArmorHelper.java
@@ -0,0 +1,21 @@
+package net.shirojr.titanfabric.util.items;
+
+import java.util.List;
+import java.util.stream.IntStream;
+
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.shirojr.titanfabric.item.custom.armor.NetherArmorItem;
+
+public class ArmorHelper {
+
+    public static List<Item> getArmorItems(PlayerEntity playerEntity) {
+        return IntStream.rangeClosed(0, 3).mapToObj(playerEntity.getInventory()::getArmorStack).map(ItemStack::getItem).toList();
+    }
+
+    public static int getNetherArmorCount(PlayerEntity playerEntity) {
+        return (int) getArmorItems(playerEntity).stream().filter(item -> item instanceof NetherArmorItem).count();
+    }
+
+}
