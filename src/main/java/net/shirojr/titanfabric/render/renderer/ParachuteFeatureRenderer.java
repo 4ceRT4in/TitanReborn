7af6351261f0836f1e1b005b0cba18f93782136c
JR1811 <janreichelt77@gmail.com>
2023-12-17 10:57:56 +0100

    small ParachuteFeatureRenderer clean-up

--- src/main/java/net/shirojr/titanfabric/render/renderer/ParachuteFeatureRenderer.java
+++ src/main/java/net/shirojr/titanfabric/render/renderer/ParachuteFeatureRenderer.java
@@ -15,29 +15,26 @@ import net.shirojr.titanfabric.item.TitanFabricItems;
 import net.shirojr.titanfabric.render.model.ParachuteFeatureModel;
 import net.shirojr.titanfabric.util.LoggerUtil;
 
-public class ParachuteFeatureRenderer<T extends LivingEntity, M extends BipedEntityModel<T>, A extends BipedEntityModel<T>> extends FeatureRenderer<T, M> {
+public class ParachuteFeatureRenderer<T extends LivingEntity, M extends BipedEntityModel<T>, A extends BipedEntityModel<T>>
+        extends FeatureRenderer<T, M> {
     private static final Identifier PARACHUTE_TEXTURE = new Identifier(TitanFabric.MODID, "textures/entity/parachute/parachute.png");
     public static ParachuteFeatureModel parachuteModel;
 
     public ParachuteFeatureRenderer(FeatureRendererContext<T, M> context, EntityModelLoader loader) {
         super(context);
         parachuteModel = new ParachuteFeatureModel(ParachuteFeatureModel.getModelData().createModel());
-        //  loader.getModelPart(TitanFabricModelLayers.PARACHUTE)
     }
 
     @Override
-    public void render(MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, T entity, float limbAngle, float limbDistance, float tickDelta, float animationProgress, float headYaw, float headPitch) {
-        LoggerUtil.devLogger(String.valueOf(tickDelta));
-
+    public void render(MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, T entity, float limbAngle,
+                       float limbDistance, float tickDelta, float animationProgress, float headYaw, float headPitch) {
         if (!entity.getMainHandStack().isOf(TitanFabricItems.PARACHUTE) &&
                 !entity.getOffHandStack().isOf(TitanFabricItems.PARACHUTE)) return;
         matrices.push();
-
-        //matrices.multiply(Vec3f.POSITIVE_Y.getDegreesQuaternion(n));
-
         matrices.scale(4, 4, 4);
         matrices.translate(0, -1.1, 0);
-        parachuteModel.render(matrices, vertexConsumers.getBuffer(RenderLayer.getEntityCutoutNoCull(PARACHUTE_TEXTURE)), light, OverlayTexture.DEFAULT_UV, 1, 1, 1, 1);
+        parachuteModel.render(matrices, vertexConsumers.getBuffer(RenderLayer.getEntityCutoutNoCull(PARACHUTE_TEXTURE)),
+                light, OverlayTexture.DEFAULT_UV, 1, 1, 1, 1);
         matrices.pop();
     }
 }
