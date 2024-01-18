5f31f1ee3f42cce466653f3c101a5e5ba9515868
Globox1997 <mitschke11@web.de>
2024-01-18 22:59:21 +0100

    tweaked parachute feature

--- src/main/java/net/shirojr/titanfabric/item/custom/TitanFabricParachuteItem.java
+++ src/main/java/net/shirojr/titanfabric/item/custom/TitanFabricParachuteItem.java
@@ -1,11 +1,55 @@
 package net.shirojr.titanfabric.item.custom;
 
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NbtCompound;
+import net.minecraft.util.Hand;
+import net.minecraft.util.TypedActionResult;
+import net.minecraft.world.World;
+import net.shirojr.titanfabric.item.TitanFabricItems;
 
 public class TitanFabricParachuteItem extends Item {
+
     public TitanFabricParachuteItem(Settings settings) {
         super(settings);
     }
 
+    @Override
+    public void inventoryTick(ItemStack stack, World world, Entity entity, int slot, boolean selected) {
+        super.inventoryTick(stack, world, entity, slot, selected);
+        if (world.getTime() % 20 == 0 && entity instanceof PlayerEntity playerEntity && playerEntity.isOnGround() && stack.getNbt() != null && stack.getNbt().contains("Activated")
+                && stack.getNbt().getBoolean("Activated")) {
+            NbtCompound nbtCompound = stack.getOrCreateNbt();
+            nbtCompound.putBoolean("Activated", false);
+            stack.setNbt(nbtCompound);
+        }
+    }
+
+    @Override
+    public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
+        if (!user.isOnGround()) {
+            NbtCompound nbtCompound = user.getStackInHand(hand).getOrCreateNbt();
+            nbtCompound.putBoolean("Activated", true);
+            user.getStackInHand(hand).setNbt(nbtCompound);
+        }
+        return super.use(world, user, hand);
+
+    }
+
+    public static boolean isParachuteActivated(LivingEntity entity) {
+        if (entity.getMainHandStack().isOf(TitanFabricItems.PARACHUTE)) {
+            if (entity.getMainHandStack().getNbt() != null && entity.getMainHandStack().getNbt().contains("Activated") && entity.getMainHandStack().getNbt().getBoolean("Activated")) {
+                return true;
+            }
+        } else if (entity.getOffHandStack().isOf(TitanFabricItems.PARACHUTE)) {
+            if (entity.getOffHandStack().getNbt() != null && entity.getOffHandStack().getNbt().contains("Activated") && entity.getOffHandStack().getNbt().getBoolean("Activated")) {
+                return true;
+            }
+        }
+        return false;
+    }
 
 }
--- src/main/java/net/shirojr/titanfabric/mixin/LivingEntityMixin.java
+++ src/main/java/net/shirojr/titanfabric/mixin/LivingEntityMixin.java
@@ -1,31 +1,23 @@
 package net.shirojr.titanfabric.mixin;
 
-import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.effect.StatusEffect;
 import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.Vec3d;
-import net.shirojr.titanfabric.effect.TitanFabricStatusEffects;
+import net.shirojr.titanfabric.item.custom.TitanFabricParachuteItem;
 import net.shirojr.titanfabric.item.custom.armor.CitrinArmorItem;
-import net.shirojr.titanfabric.item.custom.armor.NetherArmorItem;
-import net.shirojr.titanfabric.util.effects.EffectHelper;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.Redirect;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 import java.util.List;
 import java.util.Map;
@@ -46,23 +38,22 @@ public abstract class LivingEntityMixin {
     @Shadow
     protected abstract void onStatusEffectApplied(StatusEffectInstance effect, @Nullable Entity source);
 
-    @Inject(method = "addStatusEffect(Lnet/minecraft/entity/effect/StatusEffectInstance;Lnet/minecraft/entity/Entity;)Z",
-            cancellable = true, at = @At("HEAD"))
+    @Inject(method = "addStatusEffect(Lnet/minecraft/entity/effect/StatusEffectInstance;Lnet/minecraft/entity/Entity;)Z", cancellable = true, at = @At("HEAD"))
     private void titanfabric$addStatusEffect(StatusEffectInstance effect, Entity source, CallbackInfoReturnable<Boolean> cir) {
         Entity entity = (LivingEntity) (Object) this;
-        if (!(entity instanceof PlayerEntity player)) return;
-        if (!canHaveStatusEffect(effect)) return;
+        if (!(entity instanceof PlayerEntity player))
+            return;
+        if (!canHaveStatusEffect(effect))
+            return;
 
-        List<StatusEffect> blockedEffectList = List.of(StatusEffects.BLINDNESS, StatusEffects.POISON,
-                StatusEffects.WEAKNESS, StatusEffects.WITHER, StatusEffects.SLOWNESS);
+        List<StatusEffect> blockedEffectList = List.of(StatusEffects.BLINDNESS, StatusEffects.POISON, StatusEffects.WEAKNESS, StatusEffects.WITHER, StatusEffects.SLOWNESS);
 
         for (var entry : blockedEffectList) {
-            if (!blockedEffectList.contains(effect.getEffectType())) return;
+            if (!blockedEffectList.contains(effect.getEffectType()))
+                return;
         }
 
-        List<Item> armorSet = IntStream.rangeClosed(0, 3)
-                .mapToObj(player.getInventory()::getArmorStack)
-                .map(ItemStack::getItem).toList();
+        List<Item> armorSet = IntStream.rangeClosed(0, 3).mapToObj(player.getInventory()::getArmorStack).map(ItemStack::getItem).toList();
 
         if (armorSet.stream().allMatch(item -> item instanceof CitrinArmorItem)) {
             cir.setReturnValue(false);
@@ -78,18 +69,17 @@ public abstract class LivingEntityMixin {
 
         int effectDuration;
 
-        //TODO: helper class
+        // TODO: helper class
         switch (itemCounter) {
-            case 1 -> effectDuration = (int) (effect.getDuration() * 0.75);
-            case 2 -> effectDuration = (int) (effect.getDuration() * 0.5);
-            case 3 -> effectDuration = (int) (effect.getDuration() * 0.25);
-            case 4 -> effectDuration = 0;
-            default -> effectDuration = effect.getDuration();
+        case 1 -> effectDuration = (int) (effect.getDuration() * 0.75);
+        case 2 -> effectDuration = (int) (effect.getDuration() * 0.5);
+        case 3 -> effectDuration = (int) (effect.getDuration() * 0.25);
+        case 4 -> effectDuration = 0;
+        default -> effectDuration = effect.getDuration();
         }
 
         StatusEffectInstance statusEffectInstance = activeStatusEffects.get(effect.getEffectType());
-        StatusEffectInstance newStatusEffectInstance = new StatusEffectInstance(effect.getEffectType(),
-                effectDuration, effect.getAmplifier(), effect.isAmbient(), effect.shouldShowParticles(),
+        StatusEffectInstance newStatusEffectInstance = new StatusEffectInstance(effect.getEffectType(), effectDuration, effect.getAmplifier(), effect.isAmbient(), effect.shouldShowParticles(),
                 effect.shouldShowIcon());
 
         if (statusEffectInstance == null) {
@@ -105,14 +95,14 @@ public abstract class LivingEntityMixin {
         cir.setReturnValue(false);
     }
 
-    @Inject(method = "travel", at = @At(value = "FIELD",
-            target = "Lnet/minecraft/entity/effect/StatusEffects;SLOW_FALLING:Lnet/minecraft/entity/effect/StatusEffect;",
-            shift = At.Shift.BEFORE, by = 1), locals = LocalCapture.CAPTURE_FAILHARD)
-    private void titanfabric$handleSafeLandingEffect(Vec3d movementInput, CallbackInfo ci, double downForce, boolean test) {
-        LivingEntity entity = (LivingEntity)(Object) this;
-        if (!entity.hasStatusEffect(TitanFabricStatusEffects.SAFE_FALLING) || entity.getVelocity().y > 0.0) return;
-        downForce = 0.01;
-
-        entity.onLanding();
+    @ModifyVariable(method = "travel", at = @At(value = "FIELD", target = "Lnet/minecraft/entity/effect/StatusEffects;SLOW_FALLING:Lnet/minecraft/entity/effect/StatusEffect;", shift = At.Shift.BEFORE, by = 1), ordinal = 0)
+    private double titanfabric$handleSafeLandingEffect(double original) {
+        LivingEntity entity = (LivingEntity) (Object) this;
+        if (entity.getVelocity().getY() < 0 && TitanFabricParachuteItem.isParachuteActivated(entity)) {
+            original = 0.01D;
+            entity.onLanding();
+        }
+        return original;
     }
+
 }
