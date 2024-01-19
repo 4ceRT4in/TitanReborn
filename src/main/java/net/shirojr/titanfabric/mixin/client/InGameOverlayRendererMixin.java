acb77438cdcbb7928b723fabf1cd9cead8fe6fd3
4ceRT4in <Justinbiehl.JB@googlemail.com>
2024-01-19 19:10:11 +0100

    improved perfomance for HUD rendering

--- src/main/java/net/shirojr/titanfabric/mixin/client/InGameOverlayRendererMixin.java
+++ src/main/java/net/shirojr/titanfabric/mixin/client/InGameOverlayRendererMixin.java
@@ -1,17 +1,17 @@
 package net.shirojr.titanfabric.mixin.client;
 
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.hud.InGameOverlayRenderer;
 import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.item.Item;
 import net.shirojr.titanfabric.item.custom.armor.NetherArmorItem;
 import net.shirojr.titanfabric.util.items.ArmorHelper;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Environment(EnvType.CLIENT)
 @Mixin(InGameOverlayRenderer.class)
@@ -20,8 +20,9 @@ public abstract class InGameOverlayRendererMixin {
     // From AdventureZ
     @Inject(method = "renderOverlays", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/network/ClientPlayerEntity;isOnFire()Z"), cancellable = true)
     private static void fireOverlayMixin(MinecraftClient minecraftClient, MatrixStack matrixStack, CallbackInfo info) {
-        if (minecraftClient.player != null && (int) ArmorHelper.getArmorItems(minecraftClient.player).stream().filter(item -> item instanceof NetherArmorItem).count() == 4) {
-            info.cancel();
+        if (minecraftClient.player == null) return;
+        for (Item entry : ArmorHelper.getArmorItems(minecraftClient.player)) {
+            if (!(entry instanceof NetherArmorItem)) info.cancel();
         }
     }
 }
