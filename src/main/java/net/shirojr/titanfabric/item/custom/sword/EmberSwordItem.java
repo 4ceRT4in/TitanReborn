16fa270d9abbbb4baa1f55913d6da93accf89874
JR1811 <janreichelt77@gmail.com>
2024-03-02 01:37:46 +0100

    small cleanup

--- src/main/java/net/shirojr/titanfabric/item/custom/sword/EmberSwordItem.java
+++ src/main/java/net/shirojr/titanfabric/item/custom/sword/EmberSwordItem.java
@@ -5,8 +5,8 @@ import net.minecraft.item.ToolMaterial;
 import net.shirojr.titanfabric.item.custom.TitanFabricSwordItem;
 import net.shirojr.titanfabric.util.effects.WeaponEffect;
 
-public class NetherSwordItem extends TitanFabricSwordItem {
-    public NetherSwordItem(boolean hasWeaponEffects, ToolMaterial toolMaterial, int attackDamage, float attackSpeed, Item.Settings settings) {
+public class EmberSwordItem extends TitanFabricSwordItem {
+    public EmberSwordItem(boolean hasWeaponEffects, ToolMaterial toolMaterial, int attackDamage, float attackSpeed, Item.Settings settings) {
         super(hasWeaponEffects, toolMaterial, attackDamage, attackSpeed, WeaponEffect.FIRE, settings);
     }
 }
