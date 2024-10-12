5a510058c96d20ac5c96c31e85baa6059f89704f
cuzimfab@web.de <Lollollol2>
2024-10-12 15:46:58 +0200

    added cooldown for lingering potions

---
+++ src/main/java/net/shirojr/titanfabric/mixin/LingeringPotionItemMixin.java
@@ -0,0 +1,29 @@
+package net.shirojr.titanfabric.mixin;
+
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.LingeringPotionItem;
+import net.minecraft.item.PotionItem;
+import net.minecraft.item.SplashPotionItem;
+import net.minecraft.util.Hand;
+import net.minecraft.util.TypedActionResult;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(LingeringPotionItem.class)
+public abstract class LingeringPotionItemMixin extends PotionItem {
+
+    private LingeringPotionItemMixin(Settings settings) {
+        super(settings);
+    }
+
+    @Inject(method = "use", at = @At("RETURN"))
+    private void onUse(World world, PlayerEntity user, Hand hand,
+                       CallbackInfoReturnable<TypedActionResult<ItemStack>> info) {
+        // Add a cooldown to prevent splash damage/health from becoming too overpowered when stackable
+        user.getItemCooldownManager().set(this, 20 * 10);
+    }
+}
