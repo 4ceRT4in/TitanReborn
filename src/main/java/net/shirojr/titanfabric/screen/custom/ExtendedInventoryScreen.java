be5e14a0d3fddac3afa2be9389bfe5be405ccbb9
JR1811 <janreichelt77@gmail.com>
2025-05-25 04:07:51 +0200

    continued working on porting to 1.21.1 (WIP!)

--- src/main/java/net/shirojr/titanfabric/screen/custom/ExtendedInventoryScreen.java
+++ src/main/java/net/shirojr/titanfabric/screen/custom/ExtendedInventoryScreen.java
@@ -1,22 +1,18 @@
 package net.shirojr.titanfabric.screen.custom;
 
-import com.mojang.blaze3d.systems.RenderSystem;
+import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.screen.ingame.CreativeInventoryScreen;
 import net.minecraft.client.gui.screen.ingame.HandledScreen;
 import net.minecraft.client.gui.screen.ingame.InventoryScreen;
 import net.minecraft.client.gui.widget.ButtonWidget;
-import net.minecraft.client.render.GameRenderer;
-import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.player.PlayerInventory;
-import net.minecraft.text.LiteralText;
 import net.minecraft.text.Text;
-import net.minecraft.text.TranslatableText;
 import net.minecraft.util.Identifier;
 import net.shirojr.titanfabric.TitanFabric;
 import net.shirojr.titanfabric.screen.handler.ExtendedInventoryScreenHandler;
 
 public class ExtendedInventoryScreen extends HandledScreen<ExtendedInventoryScreenHandler> {
-    public static final Identifier TEXTURE = new Identifier(TitanFabric.MOD_ID, "textures/gui/extended_inventory.png");
+    public static final Identifier TEXTURE = TitanFabric.getId("textures/gui/extended_inventory.png");
 
     public ExtendedInventoryScreen(ExtendedInventoryScreenHandler handler, PlayerInventory inventory, Text title) {
         super(handler, inventory, title == null ? Text.of("") : title);
@@ -33,48 +29,37 @@ public class ExtendedInventoryScreen extends HandledScreen<ExtendedInventoryScre
 
         if (client == null || this.client.player == null || this.client.interactionManager == null) return;
         if (this.client.interactionManager.hasCreativeInventory()) {
-            this.client.setScreen(new CreativeInventoryScreen(this.client.player));
+            this.client.setScreen(new CreativeInventoryScreen(this.client.player, this.client.player.networkHandler.getEnabledFeatures(), this.client.options.getOperatorItemsTab().getValue()));
             return;
         }
-        ButtonWidget buttonWidget = new ButtonWidget(this.x + 2, this.height / 2 - 106,
-                20, 20, new LiteralText("<<"), button -> {
-            if(this.client.mouse != null) {
+        ButtonWidget buttonWidget = ButtonWidget.builder(Text.literal("<<"), button -> {
+            if (this.client.mouse != null) {
                 this.client.mouse.unlockCursor();
             }
             this.client.setScreen(new InventoryScreen(this.client.player));
-        });
+        }).dimensions(this.x + 2, this.height / 2 - 106, 20, 20).build();
         this.addDrawableChild(buttonWidget);
     }
 
     @Override
-    protected void drawBackground(MatrixStack matrices, float delta, int mouseX, int mouseY) {
-        RenderSystem.setShader(GameRenderer::getPositionTexShader);
-        RenderSystem.setShaderColor(1.0f, 1.0f, 1.0f, 1.0f);
-        RenderSystem.setShaderTexture(0, TEXTURE);
-        drawTexture(matrices, x, y, 0, 0, backgroundWidth, backgroundHeight);
+    protected void drawBackground(DrawContext context, float delta, int mouseX, int mouseY) {
         if (this.client == null || this.client.player == null) return;
-        InventoryScreen.drawEntity(x + 51, y + 75, 30,
-                (float) (x + 51) - mouseX, (float) (y + 75 - 50) - mouseY,
-                this.client.player);
+        context.drawTexture(TEXTURE, x, y, 0, 0, this.backgroundWidth, this.backgroundHeight);
+        InventoryScreen.drawEntity(context, x + 26, y + 8, x + 75, y + 78, 30, 0.0625F, mouseX, mouseY, this.client.player);
     }
 
     @Override
-    protected void drawForeground(MatrixStack matrices, int mouseX, int mouseY) {
-        int color = 0x404040;
-
-        this.textRenderer.draw(matrices, new TranslatableText("screen.titanfabric.extended_inventory.title"), this.titleX, this.titleY, color);
-/*        for (int line = 0; line < this.handler.getDescription().size(); line++) {
-            int yOffset = 52 + (line * 10);
-            Text lineText = new LiteralText(this.handler.getDescription().get(line));
-            this.textRenderer.draw(matrices, lineText, this.titleX, this.titleY + yOffset, color);
-        }*/
+    protected void drawForeground(DrawContext context, int mouseX, int mouseY) {
+        super.drawForeground(context, mouseX, mouseY);
+        context.drawText(this.textRenderer, Text.translatable("screen.titanfabric.extended_inventory.title"),
+                this.titleX, this.titleY, 0x404040, false);
     }
 
     @Override
-    public void render(MatrixStack matrices, int mouseX, int mouseY, float delta) {
-        renderBackground(matrices);
-        super.render(matrices, mouseX, mouseY, delta);
-        drawMouseoverTooltip(matrices, mouseX, mouseY);
+    public void render(DrawContext context, int mouseX, int mouseY, float delta) {
+        this.renderBackground(context, mouseX, mouseY, delta);
+        super.render(context, mouseX, mouseY, delta);
+        this.drawMouseoverTooltip(context, mouseX, mouseY);
     }
 }
 
