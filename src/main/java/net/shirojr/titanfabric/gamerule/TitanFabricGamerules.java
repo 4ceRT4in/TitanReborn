be7ddd1ae814ce9067c22e6257c9764439482ad2
JR1811 <janreichelt77@gmail.com>
2025-07-20 11:17:12 +0200

    removed unused gamerule

--- src/main/java/net/shirojr/titanfabric/gamerule/TitanFabricGamerules.java
+++ src/main/java/net/shirojr/titanfabric/gamerule/TitanFabricGamerules.java
@@ -5,11 +5,10 @@ import net.fabricmc.fabric.api.gamerule.v1.GameRuleRegistry;
 import net.minecraft.world.GameRules;
 
 public class TitanFabricGamerules {
-
     public static final GameRules.Key<GameRules.BooleanRule> LEGACY_COMBAT = registerBooleanGamerule("legacyCombat", GameRules.Category.PLAYER, true);
-    public static final GameRules.Key<GameRules.BooleanRule> LEGACY_ENCHANTMENTS = registerBooleanGamerule("legacyEnchantments", GameRules.Category.MISC, true);
     public static final GameRules.Key<GameRules.BooleanRule> LEGACY_FOOD_REGENERATION = registerBooleanGamerule("legacyFoodRegeneration", GameRules.Category.PLAYER, true);
 
+    @SuppressWarnings("SameParameterValue")
     private static GameRules.Key<GameRules.BooleanRule> registerBooleanGamerule(String name, GameRules.Category category, boolean defaultValue) {
         return GameRuleRegistry.register(name, category, GameRuleFactory.createBooleanRule(defaultValue));
     }
